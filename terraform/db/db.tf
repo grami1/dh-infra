73ce3188bc15ba947603f3bdeaf6262a0ccac9b9
grami1 <swdevnode@gmail.com>
2023-08-31 16:33:42 +0200

    add DB deployment

---
+++ terraform/db/db.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = var.region
+}
+
+resource "aws_dynamodb_table" "dh_events_table" {
+  name           = "dh-events"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "sensorId"
+
+  attribute {
+    name = "sensorId"
+    type = "S"
+  }
+}
